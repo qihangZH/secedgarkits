b747d67680f2c03a0d44db0f58351e16b0c541d6
qihangZH <694499657@qq.com>
2024-01-12 19:38:58 +0800

    Update regex of 10K parser for better check the href if contains.

--- secedgarkits/ParserT.py
+++ secedgarkits/ParserT.py
@@ -10,8 +10,12 @@ def parser_10k_df(html_str):
     """
 
     # Write the regex
-    regex = re.compile(r'((\<[^\<\>]+>)(Item|ITEM)(\s|&#160;|&nbsp;)(\d+[A-Z]{0,})\.{0,1})',
-                       flags=re.IGNORECASE)
+    # regex = re.compile(r'((\<[^\<\>]+>)(Item|ITEM)(\s|&#160;|&nbsp;)(\d+[A-Z]{0,})\.{0,1})',
+    #                    flags=re.IGNORECASE)
+    """new version: match the outsiders but not the first tags"""
+    regex = re.compile(
+        r'\<\/[^\<\>]+\>((\<[^\/][^\<\>]*\>)+)(item)(\s|&#160;|&nbsp;)(\d+[A-Z]{0,})\.{0,1}',
+        flags=re.IGNORECASE)
 
     # Use finditer to math the regex
     matches = regex.finditer(html_str)
@@ -19,9 +23,9 @@ def parser_10k_df(html_str):
     test_df = pd.DataFrame(
         [
             (
-                bool(re.search(r'(href|onclick)\=', x.groups()[1], flags=re.IGNORECASE)),
-                x.groups()[4],
-                x.start(),
+                bool(re.search(r'(href|onclick)\=', x.groups()[0], flags=re.IGNORECASE)),
+                x.groups()[-1],
+                x.start(1),
                 x.end()
             )
             for x in matches
@@ -38,8 +42,8 @@ def parser_10k_df(html_str):
     # remove all the items that contain links
     noindex_nolink_test_df = noindex_test_df[~noindex_test_df['contain_links']].sort_values('start', ascending=True)
 
-    if noindex_nolink_test_df.duplicated(subset=['item_num']).any():
-        print("PARSE ERROR, There are duplicated item numbers with out link(href/onclick) in the 10-K Form.")
+    assert not noindex_nolink_test_df.duplicated(subset=['item_num']).any(), \
+        "PARSE ERROR, There are duplicated item numbers with out link(href/onclick) in the 10-K Form."
 
     noindex_nolink_test_df['end_index'] = noindex_nolink_test_df['start'].shift(-1).astype(pd.Int64Dtype())
 

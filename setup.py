8b218a54c0915f6490d2cae460f381b1be174c36
qihangZH <694499657@qq.com>
2024-03-18 16:00:08 +0800

    Update to install easily

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='secedgarkits',
+    version='0.1_alpha',
+    packages=find_packages(),
+    author="Qihang Zhang, NUS business school",
+    author_email="e0952154@u.nus.edu",
+    description="secedgar kits for accounting research",
+    long_description=open('README.md').read(),
+    url="https://github.com/qihangZH/secedgarkits",
+)
